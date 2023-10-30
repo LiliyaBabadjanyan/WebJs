8b9a4f9234b8c1318b48ce8819b02f7c0d6c8130
LiliyaBabadjanyan <liliyafrein380@gmail.com>
2023-10-31 01:08:54 +0300

    first commit1

--- index.js
+++ index.js
@@ -1,20 +1,83 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM loaded and parsed");
-});
+const prices = {
+    defPrice: [60, 70, 80],
+    radio: {
+        r: 1,
+        r1: 2,
+        r2: 3
+    },
+    check: 50
+};
 
-const prices = [100, 200, 30];
 
-function onClick() {
-    const current = document.getElementById('select');
+function update() {
+    let select = document.getElementById("select");
+    let res = 0;
+    let rd = document.getElementById("radios");
+    let cd = document.getElementById("checkD");
+
+    switch (select.value) {
+        case "0":
+            rd.style.display = "block";
+            cd.style.display = "flex";
+            res = prices.defPrice[parseInt(select.value)];
+            break;
+        case "1":
+            rd.style.display = "block";
+            cd.style.display = "none";
+            res = prices.defPrice[parseInt(select.value)];
+            break;
+        case "2":
+            rd.style.display = "none";
+            cd.style.display = "flex";
+            res = prices.defPrice[parseInt(select.value)];
+            break;
+        default:
+            alert("не выбранно");
+    }
+
+
+    let radio = document.getElementsByName("radio");
+    radio.forEach((e) => {
+        if (rd.style.display === "block"){
+            if (e.checked) {
+                let price = prices.radio[e.value];
+                res *= price;
+            }
+        }
+    });
+
+    let check = document.getElementById("checkbox");
+
+    if(cd.style.display === "flex") {
+        if (check.checked) {
+            res +=prices.check;
+        }
+    }
+
     const count = document.getElementById('Count');
-    let price = document.getElementById('result');
-    if (/^\d+$/.test(count.value)) {
-        price.value = prices[current.value] * count.value + '₽';
-    }
-    else {
-        alert('для ввода доступны только числа');
-    }
+    res = res*count.value;
+
+    let endPrice = document.getElementById("result");
+    endPrice.value = res;
 }
 
-const btn = document.getElementById('button');
-btn.addEventListener('click', onClick);
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded and parsed");
+    let s = document.getElementById("select");
+    s.addEventListener("change", update);
+
+    const c = document.getElementById('Count');
+    c.addEventListener('change', update)
+
+    let r = document.getElementsByName("radio");
+    r.forEach((e) => {
+        e.addEventListener("change", update);
+    });
+
+    let check = document.getElementById("checkbox");
+    check.addEventListener("change", update);
+
+});
+
+update();
+
